6e2fe27702f04af1141d74edb1ccfc68234adf20
Osvaldo Galán <osvaldogalan7@gmail.com>
2026-05-19 23:51:08 -0300

    agregue cambios en los middleware e hice las validaciones del modelo post

--- src/db/routers/postRoutes.js
+++ src/db/routers/postRoutes.js
@@ -1,14 +1,20 @@
 const express = require("express");
+const {Post, Postimage} = require("../db/models");
 const { createPost, getPosts, updatePost, deletePost, addImage, removeImage } = require("../controllers/postController.js");
-const  validarPost  = require ("../middlewares/validarPost.js");
+const middleware = require("../middlewares/existe.middleware.js");
+const schemaValidator = require("../middlewares/schemaValidator.js");
+const postSchema = require("../schema/postSchema.js");
 
 const router = express.Router();
 
-router.post('/', validarPost, createPost);
+router.post('/', schemaValidator(postSchema),createPost);
 router.get('/', getPosts);
-router.put('/:id', updatePost);
-router.delete('/:id', deletePost);
-router.post('/:id/images', addImage);
-router.delete('/:id/images/:imageId', removeImage);
+router.put('/:id',middleware.validaPathParameterMiddleware, middleware.validaExisteMiddleware(Post), updatePost);
+router.delete('/:id',middleware.validaPathParameterMiddleware, middleware.validaExisteMiddleware(Post), deletePost);
+router.post('/:id/images',middleware.validaPathParameterMiddleware, middleware.validaExisteMiddleware(Postimage), addImage);
+router.delete('/:id/images/:imageId',middleware.validaPathParameterMiddleware, middleware.validaExisteMiddleware(Postimage), removeImage);
 
 module.exports = router;
+
+
+
